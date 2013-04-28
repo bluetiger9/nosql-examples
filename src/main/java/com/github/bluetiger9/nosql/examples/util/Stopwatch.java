e98d704b8477b34fa397f21391cc0cc72b867662
Attila Tőkés <tokes_atti@yahoo.com>
2013-04-28 20:40:58 +0300

    Added some Maven dependencies.

--- src/main/java/com/github/bluetiger9/nosql/examples/util/Stopwatch.java
+++ src/main/java/com/github/bluetiger9/nosql/examples/util/Stopwatch.java
@@ -1,40 +1,41 @@
 package com.github.bluetiger9.nosql.examples.util;
 
 public class Stopwatch {
-	long startTime = -1;
-	long stopTime = -1;
-	
-	public Stopwatch() {
-		reset();
-	}
-	
-	public void start() {
-		startTime = System.currentTimeMillis();
-	}
-	
-	public void stop() {
-		stopTime = System.currentTimeMillis();
-	}
-	
-	public void reset() {
-		startTime = -1;
-		stopTime = -1;
-	}
-	
-	public void restart() {
-		reset(); start();
-	}
-	
-	public long time() {
-		if (startTime == -1) 
-			return -1;
-		
-		long endTime = (stopTime != -1) ? stopTime : System.currentTimeMillis();
-		return endTime - startTime;		
-	}
-	
-	public String toString() {
-		return time() + " ms";		
-	}
+    long startTime = -1;
+    long stopTime = -1;
+
+    public Stopwatch() {
+        reset();
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        stopTime = System.currentTimeMillis();
+    }
+
+    public void reset() {
+        startTime = -1;
+        stopTime = -1;
+    }
+
+    public void restart() {
+        reset();
+        start();
+    }
+
+    public long time() {
+        if (startTime == -1)
+            return -1;
+
+        long endTime = (stopTime != -1) ? stopTime : System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    public String toString() {
+        return time() + " ms";
+    }
 
 }
